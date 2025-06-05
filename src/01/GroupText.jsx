5c471e9906d2b0eac55b4724f2fd0a3e20d5d222
pzsluna26 <pzsluna26@gmail.com>
2025-06-05 13:58:05 +0900

    react 기초

---
+++ src/01/GroupText.jsx
@@ -0,0 +1,9 @@
+function GroupText () {
+    return(
+        <div className="text-2xl bg-green-200 text-white mb-2 p-1">
+            k-digital 2기
+        </div>
+    )
+}
+
+export default GroupText
