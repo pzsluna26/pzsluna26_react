56127ddc3182f1cdce9425d198b6eeb7360d7074
pzsluna <pzsluna26@gmail.com>
2025-06-27 03:58:56 +0900

    upload

--- src/05/Lotto.jsx
+++ src/05/Lotto.jsx
@@ -1,22 +1,34 @@
 import TailBall from "../component/TailBall" 
 import TailButton from "../component/TailButton"
+import {useState} from "react"
+
 export default function Lotto() {
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6]) // 초기 번호
+
   const handleClick = () => {
-    console.log("lotto")
+    const nums = generateLottoNumbers()
+    setNumbers(nums)
   }
+
+  const generateLottoNumbers = () => {
+    const arr = []
+    while (arr.length < 6) {
+      const rand = Math.floor(Math.random() * 45) + 1
+      if (!arr.includes(rand)) arr.push(rand)
+    }
+    return arr.sort((a, b) => a - b)
+  }
+
   return (
-    <div className= "flex flex-col items-center justify-center min-h-screen">
-      <div className="flex">
-        <TailBall n="2" />
-        <TailBall n="34" />
-        <TailBall n="45" />
-        <TailBall n="23" />
-        <TailBall n="11" />
-        <TailBall n="9" />
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <div className="flex gap-2">
+        {numbers.map((n, idx) => (
+          <TailBall key={idx} n={n} />
+        ))}
       </div>
-         <TailButton caption="로또번호생성"
-                  color="blue"
-                  onHandle={handleClick}/>
+     
+        <TailButton caption="로또번호생성" color="blue" onHandle={handleClick} />
+     
     </div>
   )
 }
--- src/component/TailButton.jsx
+++ src/component/TailButton.jsx
@@ -1,15 +1,15 @@
 export default function TailButton({caption, color, onHandle}) {
   const bg = {
-    "blue" : "bg-blue-800",
-    "orange" : "bg-orange-800",
-    "lime" : "bg-lime-800"
-  }
+    blue : "bg-blue-800",
+    orange : "bg-orange-800",
+    lime : "bg-lime-800",
+  };
   return (
-    <button className={`p-4 rounded-xl text-white
+    <button className={`p-4 rounded-xl 
                        hover:cursor-pointer hover:font-bold
                       ${bg[color]}`}
             onClick={onHandle}>
       {caption}
     </button>
-  )
+  );
 }
